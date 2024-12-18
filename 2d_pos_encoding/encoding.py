870fd2ac771db667b21e66cfcaef36f45838fc5a
Dai Hang <daihang2300012956@163.com>
2024-12-18 19:58:41 +0800

    '1'

--- 2d_pos_encoding/encoding.py
+++ 2d_pos_encoding/encoding.py
@@ -1,1 +1,7 @@
-def load_picture
+def load_picture():
+    '''load a picture and return a pair of tensor/array [x,y],[r,g,b,a?]'''
+    pass
+
+def encode():
+    '''encode [x,y] -> [x,y,...]'''
+    pass
