e69c487808d387278a8e43ed69404ab04314c9c2
unknown <1490756393@qq.com>
2024-12-21 16:00:32 +0800

    encoding

--- 2d_pos_encoding/encoding.py
+++ 2d_pos_encoding/encoding.py
@@ -23,4 +23,11 @@ def encode(pos,args):        #params:L
     encode [x,y] -> [x,y,...]
     output: a tensor of encoded position, shape = (H*W),4*args.L+2
     '''
-    return torch.zeros((pos.shape[0],4*args.L+2))
+    L = args.L 
+    encoding = torch.zeros((pos.shape[0], 4 * L + 2)) 
+    for i in range(L):
+        encoding[:, 2 * i] = torch.sin(pos[:, 0] * (2 ** i))  
+        encoding[:, 2 * i + 1] = torch.cos(pos[:, 0] * (2 ** i))  
+        encoding[:, 2 * L + 2 * i] = torch.sin(pos[:, 1] * (2 ** i))  
+        encoding[:, 2 * L + 2 * i + 1] = torch.cos(pos[:, 1] * (2 ** i)) 
+    return encoding
