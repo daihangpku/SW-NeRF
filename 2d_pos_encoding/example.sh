97064b9601870bb441cd9558702cf27597048755
Dai Hang <daihang2300012956@163.com>
2024-12-27 14:02:06 +0800

    12.27

--- 2d_pos_encoding/example.sh
+++ 2d_pos_encoding/example.sh
@@ -1,1 +1,14 @@
-python 2d_pos_encoding/main.py --L 10 -pd 2d_pos_encoding/src/1.jpg  -v --epochs 200 --layer_num 12
+python 2d_pos_encoding/main.py --L 10 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 10
+python 2d_pos_encoding/main.py --L 10 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 8
+python 2d_pos_encoding/main.py --L 10 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 6
+python 2d_pos_encoding/main.py --L 8 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 10
+python 2d_pos_encoding/main.py --L 8 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 8
+python 2d_pos_encoding/main.py --L 8 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 6
+python 2d_pos_encoding/main.py --L 6 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 10
+python 2d_pos_encoding/main.py --L 6 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 8
+python 2d_pos_encoding/main.py --L 6 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 6
+python 2d_pos_encoding/main.py --L 0 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 10
+python 2d_pos_encoding/main.py --L 0 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 8
+python 2d_pos_encoding/main.py --L 0 -pd 2d_pos_encoding/src/3.jpg  -v --epochs 300 --layer_num 6
+
+
