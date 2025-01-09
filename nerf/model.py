0696ff3982ece7370217e690ffa1c9b91a9a72e7
Dai Hang <daihang2300012956@163.com>
2025-01-09 12:29:17 +0800

    '1'

--- nerf/model.py
+++ nerf/model.py
@@ -3,105 +3,89 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
+img2mse = lambda x, y : torch.mean((x - y) ** 2)
+mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 # NeRF网络模型定义
 class NeRF(nn.Module):
-   def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
-       """ 
-       参数:
-           D: 网络深度
-           W: 网络宽度(隐藏层维度)
-           input_ch: 位置编码后的输入通道数
-           input_ch_views: 视角方向编码后的输入通道数
-           output_ch: 输出通道数(RGB + density)
-           skips: 跳跃连接的层索引
-           use_viewdirs: 是否使用视角方向信息
-       """
-       super(NeRF, self).__init__()
-       self.D = D
-       self.W = W
-       self.input_ch = input_ch
-       self.input_ch_views = input_ch_views
-       self.skips = skips
-       self.use_viewdirs = use_viewdirs
-       
-       # 处理空间位置的MLP网络
-       self.pts_linears = nn.ModuleList(
-           [nn.Linear(input_ch, W)] + [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
-       
-       # 处理视角方向的MLP网络(官方实现)
-       self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W//2)])
+    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
+        """ 
+        """
+        super(NeRF, self).__init__()
+        self.D = D
+        self.W = W
+        self.input_ch = input_ch
+        self.input_ch_views = input_ch_views
+        self.skips = skips
+        self.use_viewdirs = use_viewdirs
+        
+        self.pts_linears = nn.ModuleList(
+            [nn.Linear(input_ch, W)] + [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
+        
+        ### Implementation according to the official code release (https://github.com/bmild/nerf/blob/master/run_nerf_helpers.py#L104-L105)
+        self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W//2)])
 
-       # 论文中的实现方式
-       # self.views_linears = nn.ModuleList(
-       #     [nn.Linear(input_ch_views + W, W//2)] + [nn.Linear(W//2, W//2) for i in range(D//2)])
-       
-       if use_viewdirs:
-           # 使用视角方向时的输出层
-           self.feature_linear = nn.Linear(W, W)  # 特征层
-           self.alpha_linear = nn.Linear(W, 1)    # 密度预测层
-           self.rgb_linear = nn.Linear(W//2, 3)   # RGB颜色预测层
-       else:
-           # 不使用视角方向时的输出层
-           self.output_linear = nn.Linear(W, output_ch)
+        ### Implementation according to the paper
+        # self.views_linears = nn.ModuleList(
+        #     [nn.Linear(input_ch_views + W, W//2)] + [nn.Linear(W//2, W//2) for i in range(D//2)])
+        
+        if use_viewdirs:
+            self.feature_linear = nn.Linear(W, W)
+            self.alpha_linear = nn.Linear(W, 1)
+            self.rgb_linear = nn.Linear(W//2, 3)
+        else:
+            self.output_linear = nn.Linear(W, output_ch)
 
-   def forward(self, x):
-       # 将输入分割为位置编码和视角编码
-       input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
-       h = input_pts
-       
-       # 通过位置MLP网络
-       for i, l in enumerate(self.pts_linears):
-           h = self.pts_linears[i](h)
-           h = F.relu(h)
-           if i in self.skips:
-               h = torch.cat([input_pts, h], -1)  # 跳跃连接
+    def forward(self, x):
+        input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
+        h = input_pts
+        for i, l in enumerate(self.pts_linears):
+            h = self.pts_linears[i](h)
+            h = F.relu(h)
+            if i in self.skips:
+                h = torch.cat([input_pts, h], -1)
 
-       if self.use_viewdirs:
-           # 使用视角信息的处理流程
-           alpha = self.alpha_linear(h)           # 预测密度
-           feature = self.feature_linear(h)       # 提取特征
-           h = torch.cat([feature, input_views], -1)  # 特征与视角信息拼接
-       
-           # 通过视角MLP网络
-           for i, l in enumerate(self.views_linears):
-               h = self.views_linears[i](h)
-               h = F.relu(h)
+        if self.use_viewdirs:
+            alpha = self.alpha_linear(h)
+            feature = self.feature_linear(h)
+            h = torch.cat([feature, input_views], -1)
+        
+            for i, l in enumerate(self.views_linears):
+                h = self.views_linears[i](h)
+                h = F.relu(h)
 
-           rgb = self.rgb_linear(h)              # 预测RGB颜色
-           outputs = torch.cat([rgb, alpha], -1)  # 组合最终输出
-       else:
-           # 不使用视角信息时直接输出
-           outputs = self.output_linear(h)
+            rgb = self.rgb_linear(h)
+            outputs = torch.cat([rgb, alpha], -1)
+        else:
+            outputs = self.output_linear(h)
 
-       return outputs    
+        return outputs    
 
-   def load_weights_from_keras(self, weights):
-       """从Keras模型加载预训练权重"""
-       assert self.use_viewdirs, "Not implemented if use_viewdirs=False"
-       
-       # 加载位置MLP的权重
-       for i in range(self.D):
-           idx_pts_linears = 2 * i
-           self.pts_linears[i].weight.data = torch.from_numpy(np.transpose(weights[idx_pts_linears]))    
-           self.pts_linears[i].bias.data = torch.from_numpy(np.transpose(weights[idx_pts_linears+1]))
-       
-       # 加载特征层权重
-       idx_feature_linear = 2 * self.D
-       self.feature_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_feature_linear]))
-       self.feature_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_feature_linear+1]))
+    def load_weights_from_keras(self, weights):
+        assert self.use_viewdirs, "Not implemented if use_viewdirs=False"
+        
+        # Load pts_linears
+        for i in range(self.D):
+            idx_pts_linears = 2 * i
+            self.pts_linears[i].weight.data = torch.from_numpy(np.transpose(weights[idx_pts_linears]))    
+            self.pts_linears[i].bias.data = torch.from_numpy(np.transpose(weights[idx_pts_linears+1]))
+        
+        # Load feature_linear
+        idx_feature_linear = 2 * self.D
+        self.feature_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_feature_linear]))
+        self.feature_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_feature_linear+1]))
 
-       # 加载视角MLP权重
-       idx_views_linears = 2 * self.D + 2
-       self.views_linears[0].weight.data = torch.from_numpy(np.transpose(weights[idx_views_linears]))
-       self.views_linears[0].bias.data = torch.from_numpy(np.transpose(weights[idx_views_linears+1]))
+        # Load views_linears
+        idx_views_linears = 2 * self.D + 2
+        self.views_linears[0].weight.data = torch.from_numpy(np.transpose(weights[idx_views_linears]))
+        self.views_linears[0].bias.data = torch.from_numpy(np.transpose(weights[idx_views_linears+1]))
 
-       # 加载RGB输出层权重
-       idx_rbg_linear = 2 * self.D + 4
-       self.rgb_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_rbg_linear]))
-       self.rgb_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_rbg_linear+1]))
+        # Load rgb_linear
+        idx_rbg_linear = 2 * self.D + 4
+        self.rgb_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_rbg_linear]))
+        self.rgb_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_rbg_linear+1]))
 
-       # 加载密度输出层权重
-       idx_alpha_linear = 2 * self.D + 6
-       self.alpha_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_alpha_linear]))
-       self.alpha_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_alpha_linear+1]))
+        # Load alpha_linear
+        idx_alpha_linear = 2 * self.D + 6
+        self.alpha_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_alpha_linear]))
+        self.alpha_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_alpha_linear+1]))
--- nerf/ray.py
+++ nerf/ray.py
@@ -3,26 +3,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
+img2mse = lambda x, y : torch.mean((x - y) ** 2)
+mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
+to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 def get_rays(H, W, K, c2w):
-    # H,W是图像高宽
-    # K是相机内参矩阵
-    # c2w是相机到世界坐标系的变换矩阵
-    
-    # 生成像素网格坐标
-    i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))
-    
-    # 计算射线方向
-    # 使用相机内参将像素坐标转换为归一化射线方向
+    i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))  # pytorch's meshgrid has indexing='ij'
+    i = i.t()
+    j = j.t()
     dirs = torch.stack([(i-K[0][2])/K[0][0], -(j-K[1][2])/K[1][1], -torch.ones_like(i)], -1)
-    
-    # 将射线方向从相机坐标系转换到世界坐标系
-    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)
-    
-    # 射线原点(相机中心)转换到世界坐标系
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
     rays_o = c2w[:3,-1].expand(rays_d.shape)
-    
-    return rays_o, rays_d  # 返回射线原点和方向
+    return rays_o, rays_d
+
 
 def get_rays_np(H, W, K, c2w):
     i, j = np.meshgrid(np.arange(W, dtype=np.float32), np.arange(H, dtype=np.float32), indexing='xy')
@@ -33,24 +27,26 @@ def get_rays_np(H, W, K, c2w):
     rays_o = np.broadcast_to(c2w[:3,-1], np.shape(rays_d))
     return rays_o, rays_d
 
+
 def ndc_rays(H, W, focal, near, rays_o, rays_d):
-    # 将射线原点移到近平面
+    # Shift ray origins to near plane
     t = -(near + rays_o[...,2]) / rays_d[...,2]
     rays_o = rays_o + t[...,None] * rays_d
     
-    # 投影变换
-    # 将世界坐标系下的射线转换到NDC空间
-    # NDC空间是[-1,1]³的立方体
+    # Projection
     o0 = -1./(W/(2.*focal)) * rays_o[...,0] / rays_o[...,2]
     o1 = -1./(H/(2.*focal)) * rays_o[...,1] / rays_o[...,2]
     o2 = 1. + 2. * near / rays_o[...,2]
-    
-    # 计算新的射线方向
+
     d0 = -1./(W/(2.*focal)) * (rays_d[...,0]/rays_d[...,2] - rays_o[...,0]/rays_o[...,2])
     d1 = -1./(H/(2.*focal)) * (rays_d[...,1]/rays_d[...,2] - rays_o[...,1]/rays_o[...,2])
     d2 = -2. * near / rays_o[...,2]
     
+    rays_o = torch.stack([o0,o1,o2], -1)
+    rays_d = torch.stack([d0,d1,d2], -1)
+    
     return rays_o, rays_d
+
 
 # Hierarchical sampling (section 5.2)
 def sample_pdf(bins, weights, N_samples, det=False, pytest=False):
